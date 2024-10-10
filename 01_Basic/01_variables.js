7a0bb6b1e6211abcaf95e601e0a40744eed8263f
Safwan Ahmad <123302765+mrsaffu@users.noreply.github.com>
2024-10-10 21:28:14 +0000

    variable done

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,31 @@
+/*
+ Variable
+  - It is a container where we can store the data information.
+  - There are three types of data 
+    1. Let - we can change the let Variable 
+    2. Var - its not supported block scope and functional scope. 
+    3. const - we can not change the const variable.
+
+*/
+const userId =12221
+console.log(userId)
+let acountEmail ="Saffu@google.com"
+console.log(acountEmail)
+
+var acountPasward="1234"
+acountCity="bihar"
+
+// userId=45321
+// console.log(userId)
+
+acountEmail="mrsaffu@google.come"
+console.log(acountEmail)
+
+acountPasward="23456"
+console.log(acountPasward);
+console.log(acountCity);
+
+
+
+// node 01_Basic/01_variables.js
+ 
